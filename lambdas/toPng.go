e82245a3619e5e6f06636cc8fdbee81f6c44c799
Luiggi alarcon <luiggis.ao@hotmail.com>
2019-12-19 21:24:10 -0500

    funcion convertir a PDF

--- lambdas/toPng.go
+++ lambdas/toPng.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 
 	"github.com/pkg/errors"
+	"io"
 )
 
 
@@ -31,4 +32,26 @@ func ToPng(imageBytes []byte) ([]byte, error) {
 	}
 
 	return nil, fmt.Errorf("unable to convert %#v to png", contentType)
+}
+
+func ConvertPDF(r io.Reader) (string, map[string]string, error) {
+
+	f, err := NewLocalFile(r, "/tmp", "sajari-convert-")
+	if err != nil {
+		return "", nil, fmt.Errorf("error creating local file: %v", err)
+	}
+	defer f.Done()
+
+	bodyResult, metaResult, convertErr := ConvertPDFText(f.Name())
+	if convertErr != nil {
+		return "", nil, convertErr
+	}
+	if bodyResult.err != nil {
+		return "", nil, bodyResult.err
+	}
+	if metaResult.err != nil {
+		return "", nil, metaResult.err
+	}
+	return bodyResult.body, metaResult.meta, nil
+
 }
