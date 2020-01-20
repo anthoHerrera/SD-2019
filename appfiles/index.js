8b5f5fa6584e497ca24a98cea91bcf05bcec04c8
Anthony Herrera <ankehecr100@gmail.com>
2020-01-19 23:32:52 -0500

    error require

---
+++ appfiles/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const morgan = require('morgan')
+const app = express();
+
+//settings
+app.set('port', 3000);
+app.set('view engine', 'ejs');
+//Middlewares
+app.use(express.json());
+app.use(morgan('dev'));
+
+//routes
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+});
+
+app.get('/audio', (req, res) => {
+    res.render('audio.ejs');
+});
+
+app.use(express.static('public'))
+
+app.listen(app.get('port'), () => {
+    console.log('Serve on port', app.get('port'));
+});
--- converterapp/public/javascripts/uploadAudio.js
+++ converterapp/public/javascripts/uploadAudio.js
@@ -1,46 +1,24 @@
-POLICY_JSON = {
-    "expiration": "2020-12-01T12:00:00.000Z",
-    "conditions": [
-        { "bucket": this.get('bucket') },
-        ["starts-with", "$key", ""],
-        { "acl": this.get('acl') },
-        ["starts-with", "$Content-Type", ""],
-        ["content-length-range", 0, 524288000]
-    ]
-};
 
-var secret = this.get('AWSSecretKeyId');
-var policyBase64 = Base64.encode(JSON.stringify(POLICY_JSON));
-console.log(policyBase64)
+const fs = require('fs');
+const AWS = require('aws-sdk');
 
-var signature = b64_hmac_sha1(secret, policyBase64);
-b64_hmac_sha1(secret, policyBase64);
-console.log(signature);
-
-function uploadFile() {
-
+const uploadFile = () => {
     var file = document.getElementById('file').files[0];
-    var fd = new FormData();
 
-    var key = "events/" + (new Date).getTime() + '-' + file.name;
+    const fileContent = fs.readFileSync(file);
 
-    fd.append('key', key);
-    fd.append('acl', 'public-read');
-    fd.append('Content-Type', file.type);
-    fd.append('AWSAccessKeyId', 'YOUR ACCESS KEY');
-    fd.append('policy', 'YOUR POLICY')
-    fd.append('signature', 'YOUR SIGNATURE');
 
-    fd.append("file", file);
+    const params = {
+        Bucket: '',
+        Key: file.name, 
+        Body: fileContent
+    };
 
-    var xhr = getXMLHTTPObject();
 
-    xhr.upload.addEventListener("progress", uploadProgress, false);
-    xhr.addEventListener("load", uploadComplete, false);
-    xhr.addEventListener("error", uploadFailed, false);
-    xhr.addEventListener("abort", uploadCanceled, false);
-
-    xhr.open('POST', 'https://<yourbucket>.s3.amazonaws.com/', true); //MUST BE LAST LINE BEFORE YOU SEND 
-
-    xhr.send(fd);
-}
+    s3.upload(params, function(err, data) {
+        if (err) {
+            throw err;
+        }
+        console.log('File uploaded successfully. ${data.Location}');
+    });
+};
